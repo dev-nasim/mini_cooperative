201aa3b529eca26244d3f86cf037a6b7c4868f0e
dev-nasim <letsgonasim@gmail.com>
2025-01-27 23:18:58 +0600

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cooperative;
+use App\Models\Group;
+use App\Models\Member;
+use App\Models\Transection;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('dashboard');
+    public function index()
+    {
+        $cooperatives = Cooperative::all();
+        $groups = Group::all();
+
+        $currentMonth = Carbon::now()->format('m');
+        $currentYear = Carbon::now()->format('Y');
+
+        $cooperativeTransactions = Transection::
+//            whereMonth('transaction_date', $currentMonth)
+//            ->whereYear('transaction_date', $currentYear)
+            get()
+            ->groupBy('coop_id')
+            ->map(function ($transactions, $coopId) use ($cooperatives) {
+                $cooperative = $cooperatives->find($coopId);
+                return [
+                    'cooperative_name' => $cooperative ? $cooperative->name : 'Unknown Cooperative',
+                    'saving' => $transactions->where('type', 1)->sum('amount'),
+                    'withdraw' => $transactions->where('type', 2)->sum('amount'),
+                ];
+            });
+
+        $groupTransactions = Transection::
+//            whereMonth('transaction_date', $currentMonth)
+//            ->whereYear('transaction_date', $currentYear)
+            get()
+            ->groupBy('group_id')
+            ->map(function ($transactions, $groupId) use ($groups) {
+                $group = $groups->find($groupId);
+                return [
+                    'group_name' => $group ? $group->name : 'Unknown Group',
+                    'saving' => $transactions->where('type', 1)->sum('amount'),
+                    'withdraw' => $transactions->where('type', 2)->sum('amount'),
+                ];
+            });
+
+        $totalMember = Member::where('status', 1)->count();
+        return view('dashboard', compact('cooperativeTransactions', 'groupTransactions','groups','cooperatives','totalMember'));
     }
+
+
+
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,45 @@
             <div class="h3 mb-0">Dashboard</div>
         </div>
 
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <div class="card flex-row align-items-center p-3 p-md-4">
+                    <div class="icon icon-lg bg-soft-primary rounded-circle mr-3">
+                        <i class="icon-text d-inline-block text-primary"></i>
+                    </div>
+                    <div>
+                        <h4 class="lh-1 mb-1">{{ $cooperatives->count() }}</h4>
+                        <h6 class="mb-0">Total Cooperatives</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card flex-row align-items-center p-3 p-md-4">
+                    <div class="icon icon-lg bg-soft-primary rounded-circle mr-3">
+                        <i class="gd gd-group d-inline-block text-primary"></i>
+                    </div>
+                    <div>
+                        <h4 class="lh-1 mb-1">{{ $groups->count() }}</h4>
+                        <h6 class="mb-0">Total Groups</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card flex-row align-items-center p-3 p-md-4">
+                    <div class="icon icon-lg bg-soft-primary rounded-circle mr-3">
+                        <i class="gd gd-group d-inline-block text-primary"></i>
+                    </div>
+                    <div>
+                        <h4 class="lh-1 mb-1">{{ $totalMember }}</h4>
+                        <h6 class="mb-0">Total Active Member</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <div class="row">
             <div class="col-md-6 col-xl-6 mb-3 mb-xl-6">
                 <div class="table-responsive-xl">
@@ -13,19 +52,73 @@
                         <thead class="bg-light">
                         <tr>
                             <th class="font-weight-semi-bold border-top-0 py-2">Cooperative Name</th>
-                            <th class="font-weight-semi-bold border-top-0 py-2">Total Withdraw</th>
                             <th class="font-weight-semi-bold border-top-0 py-2">Total Saving</th>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Total Withdraw</th>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Sub Total Saving</th>
                         </tr>
                         </thead>
                         <tbody>
-{{--                        @foreach ($cooperativeData as $coop)--}}
-{{--                            <tr>--}}
-{{--                                <td class="py-3">COOP-{{ $coop->coop_id }}</td>--}}
-{{--                                <td class="align-middle py-3">{{ number_format($coop->total_withdraw, 2) }}</td>--}}
-{{--                                <td class="py-3">{{ number_format($coop->total_saving, 2) }}</td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
+                        @foreach($cooperativeTransactions as $transaction)
+                            <tr>
+                                <td class="py-3">{{ $transaction['cooperative_name'] }}</td>
+                                <td class="align-middle py-3">{{ $transaction['saving'] }}</td>
+                                <td class="py-3">{{ $transaction['withdraw'] }}</td>
+                                <td>{{ $transaction['saving'] - $transaction['withdraw'] }}</td>
+                            </tr>
+                        @endforeach
                         </tbody>
+                        <tfoot>
+                        <tr>
+                            <th class="py-2">Total</th>
+                            <th class="py-2">
+                                {{ $cooperativeTransactions->sum('saving') }}
+                            </th>
+                            <th class="py-2">
+                                {{ $cooperativeTransactions->sum('withdraw') }}
+                            </th>
+                            <th class="py-2">
+                                {{ $cooperativeTransactions->sum('saving') - $cooperativeTransactions->sum('withdraw') }}
+                            </th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-6 mb-3 mb-xl-6">
+                <div class="table-responsive-xl">
+                    <table class="table text-nowrap mb-0 table-bordered">
+                        <thead class="bg-light">
+                        <tr>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Group Name</th>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Group Total Saving</th>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Group Total Withdraw</th>
+                            <th class="font-weight-semi-bold border-top-0 py-2">Last Group Total Saving</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($groupTransactions as $transaction)
+                            <tr>
+                                <td>{{ $transaction['group_name'] }}</td>
+                                <td>{{ $transaction['saving'] }}</td>
+                                <td>{{ $transaction['withdraw'] }}</td>
+                                <td>{{ $transaction['saving'] - $transaction['withdraw'] }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr class="bg-light">
+                            <th class="py-2">Total</th>
+                            <th class="py-2">
+                                {{ $groupTransactions->sum('saving') }}
+                            </th>
+                            <th class="py-2">
+                                {{ $groupTransactions->sum('withdraw') }}
+                            </th>
+                            <th class="py-2">
+                                {{ $groupTransactions->sum('saving') - $groupTransactions->sum('withdraw') }}
+                            </th>
+                        </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
